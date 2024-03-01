f1ccbe1ff5e1661b71cb608eb040fbf59cd104b8
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-02-29 22:49:51 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,165 @@
-document.body.style.backgroundColor = "#000000"; 
+document.body.style.backgroundColor = "#000000"; //set the background colors and margin
 document.body.style.margin = "0";
 
-let header = document.createElement("header");
-document.body.appendChild(header);
-header.style.width = "100%";
-header.style.height = "12vh";
+var main = document.createElement("main")
+document.body.appendChild(main)
+
+let header = document.createElement("header"); // create element header
+main.appendChild(header);
+header.style.width = "100%"; // set header bar across top of the screen
+header.style.height = "auto";
 header.style.backgroundColor = "gray";
 header.style.display= "flex"
 
-let headLogo = document.createElement("img")
+let headLogo = document.createElement("img") //create logo tag
 header.appendChild(headLogo)
-headLogo.src="./logo.png";
-headLogo.style.width = "10vh"
-headLogo.style.height = "10vh"
-headLogo.style.padding = "4px"
+headLogo.src="./logo.png"; // create logo with .src into the headLogo
+headLogo.style.width = "5rem" // modify the logo
+headLogo.style.height = "5rem"
+headLogo.style.padding = "1rem 0 1rem 1vw"
 headLogo.style.marginLeft = "1rem"
 
-let discord = document.createElement("a")
-discord.innerHTML = "Discord"
-discord.href = "https://discord.com/"
-discord.style.color = "white"
-discord.style.padding = "3.5vh 2vw 2.5rem 3vw"
-discord.style.fontSize = "2rem"
-discord.style.textDecoration = "none"
-header.appendChild(discord)
+let discord = document.createElement("a") // create first link/navigation
+discord.innerHTML = "Discord" // add text
+discord.href = "https://discord.com/" //add href into 
+discord.setAttribute("class", "nav") //set the class "nav" using set attribute
+header.appendChild(discord) // append nav lettering to nav bar
 
-let gallery = document.createElement("a")
-gallery.innerHTML = "Gallery"
+let gallery = document.createElement("a") // create second link/navigation
+gallery.innerHTML = "Gallery" // all the same as previous
 gallery.href = "https://discord.com/"
-gallery.style.color = "white"
-gallery.style.padding = "3.5vh 2vw 2.5vh 2vw"
-gallery.style.fontSize = "2rem"
-gallery.style.textDecoration = "none"
+gallery.setAttribute("class", "nav")
 header.appendChild(gallery)
 
-main = document.createElement("main")
-main.style.height = "100vh"
-document.body.appendChild(main)
+var nav = document.querySelectorAll(".nav") //use query selector to search dom for all tags with class of nav
+for(let i=0; i < nav.length; i++){ // use for loop for the length of the amount of classes
+    nav[i].style.color = "white" // systematically apply styles to all tags with class nav
+    nav[i].style.padding = "3vh 0 3vh 2vw"
+    nav[i].style.fontSize = "250%"
+    nav[i].style.textDecoration = "none"
+}
 
-var video = document.createElement("iframe");
-video.src = "https://www.youtube.com/embed/XHTrLYShBRQ?si=YShXhf154BJl5lag";
-video.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-video.setAttribute("height", "90%")
+div = document.createElement("div") //create main section
+div.style.height = "80vh"
+main.appendChild(div)
+
+var video = document.createElement("iframe"); // create iframe tag 
+video.src = "https://www.youtube.com/embed/XHTrLYShBRQ?si=YShXhf154BJl5lag"; // add link to video
+video.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"; // iframe yap
+video.setAttribute("height", "90%") // set width and height through setAttribute
 video.setAttribute("width", "90%")
-video.style.margin = "auto"
+video.style.margin = "auto" // modify video css to center it
 video.style.justifyContent = "center"
 video.style.display = "flex"
-video.style.marginTop = "2rem"
-main.appendChild(video);
+video.style.position = "relative"
+video.style.top = "3rem"
+div.appendChild(video);
+
+var footer = document.createElement("footer") //create footer tag
+footer.innerHTML = "Kuopus Incorporated | 2024 | Javascript Limited" //add footer text with .innerHTML
+footer.style.color = "white" // modify footer css
+footer.style.justifyContent = "center"
+footer.style.borderTop = "white 1px solid"
+footer.style.display = "flex"
+footer.style.fontWeight = "300"
+main.appendChild(footer) //append footer to body
+
+
+// that is all
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var secret = document.createElement("button")
+secret.setAttribute("onclick", "btn()")
+secret.style.width = "1vw"
+secret.style.height = "1vh"
+secret.style.display = "flex"
+secret.style.position = "fixed"
+secret.style.bottom = "0"
+secret.style.right = "0"
+secret.style.backgroundColor = "black"
+secret.style.border = "none"
+document.body.appendChild(secret)
+
+var trick = document.querySelector("main")
+
+var secretxt = document.createElement("div")
+secretxt.innerHTML = "You found the secret text."
+secretxt.style.display= "flex"
+secretxt.style.justifyContent = "center"
+secretxt.style.margin = "auto"
+secretxt.style.fontSize = "1000%"
+secretxt.style.color = "black"
+document.body.appendChild(secretxt)
+
+var btn = () => {
+    trick.style.display = trick.style.display == "none" ? "block" : "none"
+    document.body.style.backgroundColor = document.body.style.backgroundColor == "white" ? "black" : "white"
+    if(trick.style.display == "none"){secretxt.style.display = "block"}else{secretxt.style.display = "none"}
+}
 
 
 
